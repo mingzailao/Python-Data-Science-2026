2512d202023b879d6b98021152c1099758cfe08d
mingzialao <mingzailao@126.com>
2026-03-28 16:37:44 +0800

    no message

---
+++ deploy.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -e
+
+# Install wget
+# yum is not available on netlify, but it´s not a problem because wget is already installed, this validation is just to avoid errors on build step.
+if command -v yum &> /dev/null; then
+    yum install wget -y
+fi
+
+# Download and extract Micromamba
+wget -qO- https://micro.mamba.pm/api/micromamba/linux-64/latest | tar -xvj bin/micromamba
+
+# Set up environment variables
+export MAMBA_ROOT_PREFIX="$PWD/micromamba"
+export PATH="$PWD/bin:$PATH"
+
+# Create the environment
+micromamba create -n jupyterenv python=3.12 -c conda-forge -y
+
+# Install dependencies via pip in the micromamba environment
+micromamba run -n jupyterenv python -m pip install -r requirements-deploy.txt
+
+# Build JupyterLite
+micromamba run -n jupyterenv jupyter lite --version
+micromamba run -n jupyterenv jupyter lite build --contents content --output-dir dist
